3894ef5c34ec3ecb2657485290a0cbae75d3e825
TOM <2770573198@qq.com>
2021-01-28 23:46:21 +0800

    新增初始化sql文件

---
+++ src/main/java/pers/tom/docrepository/DocRepositoryApplication.java
@@ -0,0 +1,17 @@
+package pers.tom.docrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tom
+ * @description 文档存储库启动类
+ * @date 2021/1/28 22:09
+ */
+@SpringBootApplication
+public class DocRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocRepositoryApplication.class, args);
+    }
+}
